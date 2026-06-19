70ec0f9dc4f147fd0f8c25d21bb711a4a556d87d
Lorahk <restro481@gmail.com>
2026-06-19 22:43:50 +0530

    added search filter to watchlist

--- src/features/components/watchlist/Watchlist.jsx
+++ src/features/components/watchlist/Watchlist.jsx
@@ -4,19 +4,48 @@ import "./Watchlist.css";
 
 function Watchlist({ savedMovies, openPopup }) {
   const [currentTab, setCurrentTab] = useState("Watchlist");
+  const [localInput, setLocalInput] = useState("");
+  const [search, setSearch] = useState("");
+
+  const searchCode = /[^a-zA-Z0-9]/g;
 
   const handleWatch = () => {
     setCurrentTab(currentTab === "Watchlist" ? "Seen" : "Watchlist");
   };
 
-  const displayedMovies = savedMovies.filter(
-    (movie) => movie.sect === currentTab,
-  );
+  const displayedMovies = search
+    ? savedMovies.filter(
+        (movie) =>
+          movie.sect === currentTab &&
+          movie.Title.replace(searchCode, "").toLowerCase().includes(search),
+      )
+    : savedMovies.filter((movie) => movie.sect === currentTab);
+
+  const handleLocalInput = (e) => {
+    if (e.target.value === "") setSearch("");
+    setLocalInput(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+      setSearch(localInput.replace(searchCode, "").toLowerCase());
+    }
+  };
 
   return (
     <main className="watchlist-container">
       <header>
-        <h1>{currentTab === "Watchlist" ? "Watchlist" : "Seen"}</h1>
+        <div className="header-title-group">
+          <h1>{currentTab === "Watchlist" ? "Watchlist" : "Seen"}</h1>
+          <input
+            type="text"
+            placeholder="Filter movies..."
+            className="localSearch"
+            onChange={handleLocalInput}
+            onKeyDown={handleSearch}
+            value={localInput}
+          />
+        </div>
         <button className="watch-seen" onClick={handleWatch}>
           Switch to {currentTab === "Watchlist" ? "Seen" : "Watchlist"}
         </button>
